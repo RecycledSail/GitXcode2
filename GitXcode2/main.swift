c6533f39dd73b1728d0b95ca28fbbbec25141085
Lim Jun Young <nogurijr@naver.com>
2020-05-09 22:33:47 +0900

    Initial Commit

---
+++ GitXcode2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitXcode2
+//
+//  Created by #include on 2020/05/09.
+//  Copyright © 2020 Lim Jun Young. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
